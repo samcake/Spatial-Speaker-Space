528cc6ac223531d9562fe1c559d0fec9b3458484
Zach Fox <fox@highfidelity.io>
2021-02-22 11:23:22 -0500

    Many fixes and improvements

--- static/spatial-speaker-space/js/canvas.js
+++ static/spatial-speaker-space/js/canvas.js
@@ -1,33 +1,7 @@
 var mainCanvas = document.querySelector(".mainCanvas");
 var ctx = mainCanvas.getContext("2d");
-
-const CIRCLE_INFO = [
-    {
-        radius: 1.0,
-        color: "#DFC2F2",
-    },
-    {
-        radius: 2.5,
-        color: "#CFB3CD",
-    },
-    {
-        radius: 5.0,
-        color: "#888098",
-    },
-    {
-        radius: 10.0,
-        color: "#344055",
-    },
-];
-const CIRCLE_LABEL_PADDING_PX = 26;
-const CIRCLE_LABEL_COLOR_HEX = "#BBBBBB";
-const CIRCLE_LABEL_FONT = '18px sans-serif';
-
-const DRAW_SCALE = false;
 function updateCanvas() {
     ctx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
-
-    maybeDrawDebugCircles();
 
     if (!allUserData) {
         return;
@@ -39,6 +13,27 @@ function updateCanvas() {
     }
 
     const PIXELS_PER_METER = Math.round(Math.min(mainCanvas.width, mainCanvas.height) / VIRTUAL_SPACE_DIMENSIONS_PER_SIDE_M);
+
+    if (DRAW_SCALE) {
+        ctx.font = CIRCLE_LABEL_FONT;
+        ctx.fillStyle = CIRCLE_LABEL_COLOR_HEX;
+        ctx.lineWidth = SCALE_ARC_STROKE_WIDTH_PX;
+        for (const circleInfo of SCALE_ARC_INFO) {
+            ctx.strokeStyle = circleInfo.color;
+            let circleRadiusM = circleInfo.radius;
+            let circleRadiusPX = PIXELS_PER_METER * circleRadiusM;
+            ctx.beginPath();
+            ctx.arc(mainCanvas.width / 2, mainCanvas.height / 2, circleRadiusPX, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.textAlign = "center";
+            ctx.fillText(`${circleRadiusM}m`, mainCanvas.width / 2, mainCanvas.height / 2 - circleRadiusPX - CIRCLE_LABEL_PADDING_PX);
+            ctx.fillText(`${circleRadiusM}m`, mainCanvas.width / 2, mainCanvas.height / 2 + circleRadiusPX + CIRCLE_LABEL_PADDING_PX);
+            ctx.textAlign = "end";
+            ctx.fillText(`${circleRadiusM}m`, mainCanvas.width / 2 - circleRadiusPX - CIRCLE_LABEL_PADDING_PX, mainCanvas.height / 2);
+            ctx.textAlign = "start";
+            ctx.fillText(`${circleRadiusM}m`, mainCanvas.width / 2 + circleRadiusPX + CIRCLE_LABEL_PADDING_PX, mainCanvas.height / 2);
+        }
+    }
 
     for (const currentUserData of allUserData) {
         if (!currentUserData.position || !currentUserData.orientationEuler ||
@@ -53,71 +48,30 @@ function updateCanvas() {
         };
 
         let isMine = currentUserData.providedUserID === myProvidedUserID;
-        if (isMine && DRAW_SCALE) {
-            ctx.font = CIRCLE_LABEL_FONT;
-            ctx.fillStyle = CIRCLE_LABEL_COLOR_HEX;
-            for (const circleInfo of CIRCLE_INFO) {
-                ctx.strokeStyle = circleInfo.color;
-                let circleRadiusM = circleInfo.radius;
-                let circleRadiusPX = PIXELS_PER_METER * circleRadiusM;
-                ctx.beginPath();
-                ctx.arc(positionInCanvasSpace.x, positionInCanvasSpace.y, circleRadiusPX, 0, 2 * Math.PI);
-                ctx.stroke();
-                ctx.textAlign = "center";
-                ctx.fillText(`${circleRadiusM}m`, positionInCanvasSpace.x, positionInCanvasSpace.y - circleRadiusPX + CIRCLE_LABEL_PADDING_PX);
-                ctx.fillText(`${circleRadiusM}m`, positionInCanvasSpace.x, positionInCanvasSpace.y + circleRadiusPX - CIRCLE_LABEL_PADDING_PX);
-                ctx.textAlign = "start";
-                ctx.fillText(`${circleRadiusM}m`, positionInCanvasSpace.x - circleRadiusPX + CIRCLE_LABEL_PADDING_PX, positionInCanvasSpace.y);
-                ctx.textAlign = "end";
-                ctx.fillText(`${circleRadiusM}m`, positionInCanvasSpace.x + circleRadiusPX - CIRCLE_LABEL_PADDING_PX, positionInCanvasSpace.y);
-            }
-        }
         
         let userHexColor = hexColorFromString(currentUserData.providedUserID);        
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = 2.0;
+        ctx.lineWidth = AVATAR_STROKE_WIDTH_PX;
         ctx.fillStyle = userHexColor;
         ctx.translate(positionInCanvasSpace.x, positionInCanvasSpace.y);
         let amtToRotate = currentUserData.orientationEuler.yawDegrees * Math.PI / 180;
         ctx.rotate(amtToRotate);
         ctx.beginPath();
         ctx.arc(0, 0, USER_RADIUS_M * PIXELS_PER_METER, 0, 2 * Math.PI);
+        if (isMine) {
+            ctx.strokeStyle = MY_AVATAR_STROKE_HEX;
+        } else {
+            ctx.strokeStyle = OTHER_AVATAR_STROKE_HEX;
+        }
         ctx.stroke();
         ctx.fill();
         if (!currentUserData.isSpeaker) {
             ctx.beginPath();
-            ctx.arc(0, -(USER_RADIUS_M + 0.1) * PIXELS_PER_METER, USER_RADIUS_M / 4 * PIXELS_PER_METER, 0, 2 * Math.PI);
+            ctx.arc(0, -(USER_RADIUS_M + ORIENTATION_CIRCLE_OFFSET_M) * PIXELS_PER_METER, USER_RADIUS_M / 4 * PIXELS_PER_METER, 0, 2 * Math.PI);
+            ctx.fill();
         }
-        ctx.fill();
         ctx.rotate(-amtToRotate);
         ctx.translate(-positionInCanvasSpace.x, -positionInCanvasSpace.y);
     }
-}
-
-const DRAW_DEBUG_CIRCLES = false;
-function maybeDrawDebugCircles() {
-    if (!DRAW_DEBUG_CIRCLES) {
-        return;
-    }
-
-    const PIXELS_PER_METER = Math.round(Math.min(mainCanvas.width, mainCanvas.height) / VIRTUAL_SPACE_DIMENSIONS_PER_SIDE_M);
-    ctx.fillStyle = "#00FF00";
-
-    let positionInCanvasSpace = {
-        "x": Math.round(linearScale(0, -VIRTUAL_SPACE_DIMENSIONS_PER_SIDE_M / 2, VIRTUAL_SPACE_DIMENSIONS_PER_SIDE_M / 2, 0, mainCanvas.width)),
-        // We "reverse" the last two terms here because "-y" in canvas space is "+y" in mixer space.
-        "y": Math.round(linearScale(0, -VIRTUAL_SPACE_DIMENSIONS_PER_SIDE_M / 2, VIRTUAL_SPACE_DIMENSIONS_PER_SIDE_M / 2, mainCanvas.height, 0))
-    };
-    
-    ctx.translate(positionInCanvasSpace.x, positionInCanvasSpace.y);
-    let amtToRotate = 90 * Math.PI / 180;
-    ctx.rotate(amtToRotate);
-    ctx.beginPath();
-    ctx.arc(0, 0, USER_RADIUS_M * PIXELS_PER_METER, 0, 2 * Math.PI);
-    ctx.arc(0, -(USER_RADIUS_M + 0.1) * PIXELS_PER_METER, USER_RADIUS_M / 4 * PIXELS_PER_METER, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.rotate(-amtToRotate);
-    ctx.translate(-positionInCanvasSpace.x, -positionInCanvasSpace.y);
 }
 
 function updateCanvasDimensions() {
--- static/spatial-speaker-space/js/constants.js
+++ static/spatial-speaker-space/js/constants.js
@@ -1,2 +1,24 @@
 const USER_RADIUS_M = 0.3;
 const DEFAULT_USER_COLOR_HEX = "#FF0000";
+const CLOSE_ENOUGH_M = 0.005;
+const AUDIENCE_RADIUS_M = 4.5;
+const SPEAKER_RADIUS_M = 2;
+const DRAW_SCALE = true;
+const SCALE_ARC_STROKE_WIDTH_PX = 1.0;
+const SCALE_ARC_INFO = [
+    {
+        radius: SPEAKER_RADIUS_M,
+        color: "#DFC2F2",
+    },
+    {
+        radius: AUDIENCE_RADIUS_M,
+        color: "#CFB3CD",
+    },
+];
+const CIRCLE_LABEL_PADDING_PX = 26;
+const CIRCLE_LABEL_COLOR_HEX = "#BBBBBB";
+const CIRCLE_LABEL_FONT = '18px sans-serif';
+const MY_AVATAR_STROKE_HEX = "#FFFFFF";
+const OTHER_AVATAR_STROKE_HEX = "#000000";
+const AVATAR_STROKE_WIDTH_PX = 5.0;
+const ORIENTATION_CIRCLE_OFFSET_M = 0.15;
