7b253349a108f92d8fe2c733c8ef0ba4c0718cd7
Zach Fox <fox@highfidelity.io>
2021-02-26 19:22:36 -0500

    Try to work around some race condition websocket issues

--- static/spatial-speaker-space/js/websocketStuff.js
+++ static/spatial-speaker-space/js/websocketStuff.js
@@ -1,8 +1,5 @@
 const socket = io();
-
-function initWebSocketStuff() {
-    socket.emit("addParticipant", myProvidedUserID, spaceName);
-}
+socket.emit("addParticipant", myProvidedUserID, spaceName);
 
 function stopWebSocketStuff() {
     socket.emit("removeParticipant", myProvidedUserID, spaceName);
